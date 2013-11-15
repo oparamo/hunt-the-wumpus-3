25644b635530f3e71daa6c9c53912ece3713884c
Oscar Paramo <oscar.paramo@me.com>
2013-11-15 08:21:36 -0600

    Added Content
    Added .png textures and 3d models (spheres + cylilnders) to the content
    folder.

---
+++ Hunt the Wumpus 3/Hunt the Wumpus 3/3dClasses.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Hunt_the_Wumpus_3 {
+    //3d gameobject class
+    class GameObject {
+        public Model Model { get; set; }
+        public Vector3 Position { get; set; }
+        public bool IsActive { get; set; }
+        public BoundingSphere BoundingSphere { get; set; }
+
+        public GameObject() {
+            Model = null;
+            Position = Vector3.Zero;
+            IsActive = false;
+            BoundingSphere = new BoundingSphere();
+        }
+    }
+
+    //camera constants
+    class GameConstants {
+        public const float NearClip = 1.0f;
+        public const float FarClip = 1000.0f;
+        public const float ViewAngle = 45.0f;
+    }
+
+    //camera class
+    class Camera {
+        public Vector3 AvatarHeadOffset { get; set; }
+        public Vector3 TargetOffset { get; set; }
+        public Matrix ViewMatrix { get; set; }
+        public Matrix ProjectionMatrix { get; set; }
+
+        public Camera() {
+            AvatarHeadOffset = new Vector3(0, 7, -15);
+            TargetOffset = new Vector3(0, 5, 0);
+            ViewMatrix = Matrix.Identity;
+            ProjectionMatrix = Matrix.Identity;
+        }
+
+        public void Update(float avatarYaw, Vector3 position, float aspectRatio) {
+            Matrix rotationMatrix = Matrix.CreateRotationY(avatarYaw);
+
+            Vector3 transformedheadOffset =
+                Vector3.Transform(AvatarHeadOffset, rotationMatrix);
+            Vector3 transformedReference =
+                Vector3.Transform(TargetOffset, rotationMatrix);
+
+            Vector3 cameraPosition = position + transformedheadOffset;
+            Vector3 cameraTarget = position + transformedReference;
+
+            //Calculate the camera's view and projection 
+            //matrices based on current values.
+            ViewMatrix =
+                Matrix.CreateLookAt(cameraPosition, cameraTarget, Vector3.Up);
+            ProjectionMatrix =
+                Matrix.CreatePerspectiveFieldOfView(
+                    MathHelper.ToRadians(GameConstants.ViewAngle), aspectRatio,
+                    GameConstants.NearClip, GameConstants.FarClip);
+        }
+    }
+}
