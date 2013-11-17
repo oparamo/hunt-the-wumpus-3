e11e23da554d7d42d861bb299bb9f0027b69b33c
Oscar Paramo <oscar.paramo@me.com>
2013-11-17 00:09:43 -0600

    Added cylinder stuff.

--- Hunt the Wumpus 3/Hunt the Wumpus 3/3dClasses.cs
+++ Hunt the Wumpus 3/Hunt the Wumpus 3/3dClasses.cs
@@ -25,6 +25,25 @@ namespace Hunt_the_Wumpus_3 {
             Model = content.Load<Model>(modelName);
             Position = Vector3.Down;
         }
+
+        public void Draw(Matrix view, Matrix projection) {
+            Matrix[] transforms = new Matrix[Model.Bones.Count];
+            Model.CopyAbsoluteBoneTransformsTo(transforms);
+            Matrix translateMatrix = Matrix.CreateTranslation(Position);
+            Matrix worldMatrix = translateMatrix;
+
+            foreach (ModelMesh mesh in Model.Meshes) {
+                foreach (BasicEffect effect in mesh.Effects) {
+                    effect.World = worldMatrix * transforms[mesh.ParentBone.Index];
+                    effect.View = view;
+                    effect.Projection = projection;
+                    
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+                }
+                mesh.Draw();
+            }
+        }
     }
 
     //camera constants
